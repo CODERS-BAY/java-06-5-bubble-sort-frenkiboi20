9181c05a565ee5edd8624fca6da6622bd1ab7bd1
Jakob Penninger <jakob.penninger@gmx.net>
2020-09-07 15:00:08 +0200

    Add main

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,4 +1,38 @@
 package com.codersbay;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] unsorted = {2, 26, -12, 103};
+        int[] sorted = new int[unsorted.length];
+
+        for (int i = 0; i < unsorted.length; i++) {
+            System.out.format("[%2d]   ", unsorted[i]);
+        }
+
+        for (int i = unsorted.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (unsorted[j] > unsorted[j + 1]) {
+                    int temp = unsorted[j + 1];
+                    unsorted[j + 1] = unsorted[j];
+                    unsorted[j] = temp;
+                }
+            }
+        }
+
+        System.out.println();
+
+        ArrayList<Integer> numbList = new ArrayList<>();
+
+        for (int i = 0; i < unsorted.length; i++) {
+            System.out.format("[%2d]   ", unsorted[i]);
+            sorted[i] = unsorted[i];
+        }
+
+        System.out.println("\nSortierung beendet!");
+
+    }
 }
